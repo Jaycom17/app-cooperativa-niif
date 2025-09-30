cd277f64fb8f98c11299df25ace4d05878ae193a
Jaycom17 <juanorejuela499@gmail.com>
2025-09-30 17:02:37 -0500

    refactor: little change in github action

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { API_URL } from "./env";
 
+console.log("API_URL:", API_URL);
+
 const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
